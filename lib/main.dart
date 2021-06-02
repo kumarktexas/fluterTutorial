cb6154c17c8f59e5646695e4b67c3f3ffab9306a
Kandgule <kandgule@147dda3bc08a.ant.amazon.com>
2021-06-02 12:58:31 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        child: Text("Welcome to flutter training"),
+      ),
+    );
+  }
+}
